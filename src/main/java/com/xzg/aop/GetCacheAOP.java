8db0832b3e91163919db703dfd2b7b1c0598bddc
xiongzhenggang <ab1290014555@qq.com>
2017-03-23 09:33:36 +0800

    修改

--- src/main/java/com/xzg/aop/GetCacheAOP.java
+++ src/main/java/com/xzg/aop/GetCacheAOP.java
@@ -9,6 +9,8 @@ import java.lang.reflect.Method;
 
 
 
+
+
 import javax.annotation.Resource;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -22,11 +24,14 @@ import org.springframework.stereotype.Component;
 
 import com.xzg.cache.GetCache;
 import com.xzg.cache.RedisCache;
-
+import com.xzg.controller.RoomController;
+import com.xzg.publicUtil.CookieUtil;
+/**@AspectJ风格的切面可以通过@Compenent注解标识其为Spring管理Bean，
+而@Aspect注解不能被Spring自动识别并注册为Bean，必须通过@Component注解来完成
+*/
 @Component
-@Aspect
+@Aspect//声明该类使用了aop
 public class GetCacheAOP  {  
-      
     @Autowired
     private RedisTemplate<Serializable, Object> redisTemplate;
     @Resource(name="redisCache")
@@ -35,21 +40,17 @@ public class GetCacheAOP  {
     public void getCache(){
         System.out.println("我是一个切入点");  
     }  
-    
     /** 
      * 在所有标注@getCache的地方切入 
      * @param joinPoint 
      */
     @Around("getCache()")
     public Object beforeExec(ProceedingJoinPoint joinPoint){  
-        
-        
         //前置：到redis中查询缓存
         System.out.println("调用从redis中查询的方法...");
         
         //redis中key格式：    id
         String redisKey = getCacheKey(joinPoint);
-        
         //获取从redis中查询到的对象
         Object objectFromRedis = redisCache.getDataFromRedis(redisKey);
         
@@ -58,9 +59,7 @@ public class GetCacheAOP  {
             System.out.println("从redis中查询到了数据...不需要查询数据库");
             return objectFromRedis;
         }
-        
         System.out.println("没有从redis中查到数据...");
-        
         //没有查到，那么查询数据库
         Object object = null;
         try {
@@ -69,12 +68,9 @@ public class GetCacheAOP  {
             
             e.printStackTrace();
         }
-        
         System.out.println("从数据库中查询的数据...");
-        
         //后置：将数据库中查询的数据放到redis中
         System.out.println("调用把数据库查询的数据存储到redis中的方法...");
-        
         redisCache.setDataToRedis(redisKey, object);
         System.out.println("redis中的数据..."+object.toString());
         //将查询到的数据返回
@@ -85,27 +81,35 @@ public class GetCacheAOP  {
      * @return 格式为 "包名.类名.方法名.参数类型.参数值"，类似 "your.package.SomeService.getById(int).123"
      */
     private String getCacheKey(ProceedingJoinPoint joinPoint) {
-    
         MethodSignature ms=(MethodSignature) joinPoint.getSignature();  
         Method method=ms.getMethod();  
         String ActionName = method.getAnnotation(GetCache.class).name();  
         String fieldList = method.getAnnotation(GetCache.class).value();  
         //System.out.println("签名是"+ms.toString());
-        for (String field:fieldList.split(","))   
+        for (String field:fieldList.split(",")) //field表示输入的参数值可以有多个
              ActionName +="."+field;
         //先获取目标方法参数
         String id = null;
-        Object[] args = joinPoint.getArgs();
+        Object[] args = joinPoint.getArgs();//[10005]
         if (args != null && args.length > 0) {
             id = String.valueOf(args[0]);
         }
         ActionName += "="+id;
-        String redisKey = ms+"."+ActionName;
-        System.out.println("当前方法的key值=="+redisKey);
+        String redisKey = CookieUtil.getMD5(ms+"."+ActionName);//使用MD5加密后的作为换粗字
+        System.out.println("当前方法的key值=="+redisKey);//Object com.xzg.controller.RoomController.roomList(Integer).user.id=10005
         return redisKey;
     }
     public void setRedisTemplate(  
             RedisTemplate<Serializable, Object> redisTemplate) {  
         this.redisTemplate = redisTemplate;  
+    }
+    //
+    public static void main(String[] args){
+    	Class<RoomController> clz = RoomController.class;
+    	Method[] mt = clz.getMethods();
+    	System.out.println(mt[0]);
+    	  String ActionName	= mt[0].getAnnotation(GetCache.class).value();  
+    	System.out.println(ActionName);
+    	
     }
 }
--- src/main/java/com/xzg/cache/RedisCache.java
+++ src/main/java/com/xzg/cache/RedisCache.java
@@ -9,6 +9,7 @@ import com.xzg.publicUtil.SerializeUtil;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
 /**
  * @author hasee
@@ -34,16 +35,23 @@ public class RedisCache {
     }
     //将数据库中查询到的数据放入redis
     public void setDataToRedis(String redisKey, Object obj){
-        
         //序列化
         byte[] bytes = SerializeUtil.serialize(obj);
-        
         //存入redis
         Jedis jedis = jedisPool.getResource();
-        String success = jedis.set(redisKey.getBytes(), bytes);
-        
+        //String success = jedis.set(redisKey.getBytes(), bytes);
+       String success = jedis.setex(redisKey.getBytes(), 60, bytes);//设置缓存失效时间为60秒
         if("OK".equals(success)){
             System.out.println("数据成功保存到redis...");
         }
+    }
+    //删除redis中指定的key值
+    public void removeDataToRedis(String redisKey){
+    	try{
+    		Jedis jedis = jedisPool.getResource();
+    		jedis.del(redisKey.getBytes());
+    	}catch(JedisConnectionException e){//无法获取连接
+    		e.printStackTrace();
+    	}
     }
 }
